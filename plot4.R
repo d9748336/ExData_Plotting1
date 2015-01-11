5b2e22c86ae0559f924955d28e10ff8eb3b4d252
d9748336 <douglasrhouston@googlemail.com>
2015-01-11 17:54:19 +0000

    four

--- plot4.R
+++ plot4.R
@@ -17,7 +17,7 @@ plot(data$DateTime, data$Voltage, type = "l", ylab = "Voltage", xlab = "datetime
 plot(data$DateTime, data$Sub_metering_1, type = "l", ylab = "Energy sub metering", xlab ="", cex.lab = 0.85,  cex.axis = 0.85, cex.main = 0.8)
 lines(data$DateTime, data$Sub_metering_2, type = "l", col = 2)
 lines(data$DateTime, data$Sub_metering_3, type = "l", col = 4)
-legend("topright", lty = 1, col = c("red", "green", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), bty ="n", cex = 0.9)
+legend("topright", lty = 1, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), bty ="n", cex = 0.7)
 
 plot(data$DateTime, data$Global_reactive_power, type = "l", ylab = "Global_reactive_power", xlab = "datetime",  cex.lab = 0.85, cex.axis = 0.85, cex.main = 0.85)
 png(file = "plot4.png", width = 480, height = 480)
@@ -29,7 +29,7 @@ plot(data$DateTime, data$Voltage, type = "l", ylab = "Voltage", xlab = "datetime
 plot(data$DateTime, data$Sub_metering_1, type = "l", ylab = "Energy sub metering", xlab ="", cex.lab = 0.9,  cex.axis = 0.9, cex.main = 0.9)
 lines(data$DateTime, data$Sub_metering_2, type = "l", col = 2)
 lines(data$DateTime, data$Sub_metering_3, type = "l", col = 4)
-legend("topright", lty = 1, col = c("red", "green", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), bty ="n", cex = 0.9)
+legend("topright", lty = 1, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), bty ="n", cex = 0.9)
 
 plot(data$DateTime, data$Global_reactive_power, type = "l", ylab = "Global_reactive_power", xlab = "datetime",  cex.lab = 0.9, cex.axis = 0.9, cex.main = 0.9)
 dev.off()
